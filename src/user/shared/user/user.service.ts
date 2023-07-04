6169e60f0733df776c9caeaf0a1828d863c6402e
zenonalex <zenon_er@hotmail.com>
2023-07-04 15:42:44 -0300

    chore: create get users methods

--- src/user/shared/user/user.service.ts
+++ src/user/shared/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from '../user/user';
 import { Model } from 'mongoose';
+import { UserTypes } from './utils/user.types';
 
 @Injectable()
 export class UserService {
@@ -17,9 +18,36 @@ export class UserService {
         return this.userModel.find().exec();
     }
 
+    async getCustomers(companyId: string, psiId: string): Promise<User[]> {
+        const users = await this.getAllUsers();
+        const hasPsiIdFilter = psiId != undefined;
+
+        const filteredUsers = hasPsiIdFilter
+            ? users.filter(
+                user => user.companyId == companyId
+                    && user.psiId == psiId
+                    && user.userTypes.includes(UserTypes.CUSTOMER)
+            )
+            : users.filter(
+                user => user.companyId == companyId
+                    && user.userTypes.includes(UserTypes.CUSTOMER)
+            );
+
+        return filteredUsers;
+    }
+
+    async getPsy(companyId: string): Promise<User[]> {
+        const users = await this.getAllUsers();
+
+        return users.filter(user => {
+            return user.companyId == companyId && user.userTypes.includes(UserTypes.PSI)
+        }
+        );
+    }
+
     async getUserByEmail(email: string): Promise<User | undefined> {
         const users = await this.getAllUsers();
-        
+
         return users.find(user => user.email == email);
     }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpException, HttpStatus, Post, UseGuards, Request } from '@nestjs/common';
+import { Body, Controller, HttpException, HttpStatus, Post, UseGuards, Request, Get, Query } from '@nestjs/common';
 import { UserService } from './shared/user/user.service';
 import { User } from './shared/user/user';
 import { UserTypes } from './shared/user/utils/user.types';
@@ -19,7 +19,7 @@ export class UserController {
             }
         });
 
-        if (user.userTypes != undefined && user.name != undefined && user.email != undefined && user.companyId != undefined) {
+        if (user.userTypes != undefined && user.name != undefined && user.email != undefined) {
 
             if (user.userTypes.includes(UserTypes.CUSTOMER) && user.psiId == undefined) {
 
@@ -28,11 +28,37 @@ export class UserController {
 
             user.password = `mudar`;
             user.isFirstAccess = true;
+            user.companyId = req.user.companyId;
 
             return this.userService.createNewUser(user);
         }
 
         throw new HttpException(`name, email and userTypes are required fields`, HttpStatus.BAD_REQUEST);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get("/getCustomers")
+    async getCustomers(@Query("psiId") psiId: string, @Request() req: any): Promise<User[]> {
+
+        const hasAdminRoles = req.user.userTypes.includes(UserTypes.ADMIN);
+
+        if (hasAdminRoles) {
+            return this.userService.getCustomers(req.user.companyId, psiId);
+        }
+        return this.userService.getCustomers(req.user.companyId, undefined);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get("/getPsychologists")
+    async getPsyhcologists(@Request() req: any): Promise<User[]> {
+
+        const hasAdminRoles = req.user.userTypes.includes(UserTypes.ADMIN);
+
+        if (hasAdminRoles) {
+            return this.userService.getPsy(req.user.companyId);
+        }
+
+        throw new HttpException(`Only administrators are allowed to search to Psychologists`, HttpStatus.FORBIDDEN);
     }
 
     async getAllUsers(): Promise<User[]> {
